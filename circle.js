bb2886235eb02e88391305c59e22e195eef51c01
Hatim <hatimmarouach1@gmail.com>
2024-04-07 07:34:00 +0000

    still needs a bit of work

--- circle.js
+++ circle.js
@@ -4,9 +4,9 @@ document.addEventListener("DOMContentLoaded", function() {
         const minRotation = minutes * 6 + seconds / 10;
         const hourRotation = (hours % 12) * 30 + minutes / 2;
 
-        document.querySelector('.line1').style.transform = `translate(-50%, -50%) rotate(${secRotation+180}deg)`;
-        document.querySelector('.line2').style.transform = `translate(-50%, -50%) rotate(${minRotation+180}deg)`;
-        document.querySelector('.line3').style.transform = `translate(-50%, -50%) rotate(${hourRotation+180}deg)`;
+        document.querySelector('.line1').style.transform = `translate(-50%, -50%) rotate(${secRotation}deg)`;
+        document.querySelector('.line2').style.transform = `translate(-50%, -50%) rotate(${minRotation-80}deg)`;
+        document.querySelector('.line3').style.transform = `translate(-50%, -50%) rotate(${hourRotation-130}deg)`;
     }
 
     function updateTime() {
@@ -18,9 +18,7 @@ document.addEventListener("DOMContentLoaded", function() {
         updateRotation(hours, minutes, seconds);
     }
 
-    // Call updateTime initially to set the clock with current time
     updateTime();
 
-    // Update the clock every second
     setInterval(updateTime, 1000);
 });
